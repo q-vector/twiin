62885e4837132819ce90f31d2da2379372e0cd69
Simon Ching <e.ching@bom.gov.au>
2016-01-24 21:11:50 +1030

    Blah

--- src/twiin.h
+++ src/twiin.h
@@ -8,7 +8,8 @@
 #include <denise/geodesy.h>
 #include <denise/gtkmm.h>
 #include <andrea/andrea.h>
-#include "display.h"
+#include "data.h"
+#include "model.h"
 
 using namespace std;
 using namespace denise;
